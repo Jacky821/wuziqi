7bfc356faaa70e2f43c8124b3e5aac381876d354
Jacky <LiaoChengCheng821@gmail.com>
2020-08-17 19:06:07 +0700

    test

--- 五子棋/WZQElement.m
+++ 五子棋/WZQElement.m
@@ -18,6 +18,7 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
+        //test
         self.clickedBlock = clickedBlock;
         self.layer.cornerRadius = 5;
         self.layer.borderWidth = 1;
